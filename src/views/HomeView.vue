cfd6a6ef377ae421b3a50d8146315e8904d31d6e
Rhoda Essien <rhorose@live.com>
2022-04-02 02:25:58 -0400

    Started using Vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,26 @@
 <script>
+
+import NewsList from '@/components/NewsList.vue'
+
 export default {
     data() {
         return {
             message: "Hello World! Welcome to VueJS"
         };
     },
+  components: { NewsList }
 }
 </script>
 
 <template>
+
     <div class="container">
       <div class="text-center">
         <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
         <h1>{{ message }}</h1>
       </div>
     </div>
+    <NewsList />
 </template>
 
 <style>
